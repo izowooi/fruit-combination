0244abb63e6499c84fe2f3c1640814dd29a05535
jongwoo <izowooi@hotmail.com>
2024-12-13 08:30:55 +0900

    설정창에 메시지 추가

--- flutter_proj/lib/widget/settings_widget.dart
+++ flutter_proj/lib/widget/settings_widget.dart
@@ -2,12 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class SettingsWidget extends ConsumerWidget {
-
   SettingsWidget({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('설정'),
@@ -20,6 +18,12 @@ class SettingsWidget extends ConsumerWidget {
             subtitle: Text('1.0.0'),
           ),
           const Divider(),
+
+          // 감사 메시지
+          const ListTile(
+            title: Text('감사 메시지'),
+            subtitle: Text('계엄을 막아준 의원님들께 깊은 감사를 전합니다.'),
+          ),
         ],
       ),
     );
